e4259f449dd1cd16faaffac14d402de6c546b2e7
noble <775488842@qq.com>
2018-01-31 15:24:17 +0800

    添加了Role相关的接口代码 RoleController， IRoleService，IDmRoleDAO

--- src/main/java/cn/com/demo/mybatis/dao/IDmRoleDAO.java
+++ src/main/java/cn/com/demo/mybatis/dao/IDmRoleDAO.java
@@ -1,10 +1,29 @@
 package cn.com.demo.mybatis.dao;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import cn.com.demo.mybatis.entity.DmPowerItem;
 import cn.com.demo.mybatis.entity.DmRole;
 
 public interface IDmRoleDAO {
     public DmRole findById(int id);
+    public List<DmRole> findAll();
+    public void save(DmRole role);
+    public void update(DmRole role);
+    public void delete(DmRole role);
+    
+    public void removePowerItemsByRoleId(int rlId);
+    /**
+     * keys:
+     *   rlId
+     *   piId
+     * */
+    public void deployPowerItem(Map<String,Integer> map);
+    // 查询出角色的所有权限项
+    public List<DmPowerItem> findPowerItemsByRoleId(int rlId);
+    
+    
     public Set<DmRole> findRolesByUserId(int urId);
 }
